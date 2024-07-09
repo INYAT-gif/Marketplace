72f012dc7d04071e6c000734b0532a36757783e5
inyat <inyat.nathani@gmail.com>
2024-07-09 11:35:37 +0200

    202407091135

--- src/main/java/se/inyat/marketplace/MarketplaceApplication.java
+++ src/main/java/se/inyat/marketplace/MarketplaceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarketplaceApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MarketplaceApplication.class, args);
 	}
 
